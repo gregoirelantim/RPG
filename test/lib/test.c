77f63e3192ace8acd15ed72f0b47e889fcc9c80b
gregoire <gregoire.lan-tim@epitech.eu>
2023-02-05 18:00:05 +0100

    [ADD] menu

--- test/lib/test.c
+++ test/lib/test.c
@@ -7,6 +7,35 @@
 
 #include "../include/my.h"
 
+int display_game(glo *all)
+{
+    while (sfRenderWindow_isOpen(WINDOW)) {
+        sfRenderWindow_clear(WINDOW, sfBlack);
+        while (sfRenderWindow_pollEvent(WINDOW, &EVENT))
+            EVENT.type == sfEvtClosed ? sfRenderWindow_close(WINDOW) : 0;
+        sfRenderWindow_display(WINDOW);
+    }
+    return 0;
+}
+
+int main_game(glo *all)
+{
+    display_game(all);
+    return 0;
+}
+
+int menu_click(glo *all)
+{
+    sfVector2i mouse = sfMouse_getPositionRenderWindow(WINDOW);
+    if (mouse.x >= 802 && mouse.x <= 1100
+        && EVENT.type == sfEvtMouseButtonPressed) {
+        if (mouse.y >= 730 && mouse.y <= 977) {
+            main_game(all);
+        }
+    }
+    return 0;
+}
+
 int display_menu(glo *all)
 {
     while (sfRenderWindow_isOpen(WINDOW)) {
@@ -16,6 +45,7 @@ int display_menu(glo *all)
         for (int i = 0; i < 2; i++)
             sfRenderWindow_drawSprite(WINDOW, IMAGES[i].sprite, NULL);
         sfRenderWindow_display(WINDOW);
+        menu_click(all);
     }
     return 0;
 }
