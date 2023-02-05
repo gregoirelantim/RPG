7563779cb423be7ce34e0f7820bb6b94009ebdc8
gregoire <gregoire.lan-tim@epitech.eu>
2023-02-05 16:23:02 +0100

    [ADD] base

---
+++ my_pokemon/lib/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** my_pokemon
+** File description:
+** main
+*/
+
+#include "../include/my.h"
+
+int main(void)
+{
+    glo all;
+    return 0;
+}
---
+++ my_pokemon/lib/pokemon.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** my_pokemon
+** File description:
+** pokemon
+*/
+
+#include "../include/my.h"
+
+int pokemon(glo *all)
+{
+    return 0;
+}
